135f3351b37d49faf5b9fa85e0869fd4decc12cf
cbarb15 <cbarb2747@gmail.com>
2025-08-24 15:34:51 -0500

    added subscription to /scan for the lidar and a callback function to see if it will pring out the message

--- lane_following/lane_following/lane_following.py
+++ lane_following/lane_following/lane_following.py
@@ -1,11 +1,16 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.qos import ReliabilityPolicy, QoSProfile
 # from lane_following.lane_following.lidar_node import LidarNode
 
 class LidarNode(Node):
     def __init__(self):
         super().__init__('lidar_node')
         self.get_logger().info('Lidar Node started')
+        self.subscription = self.create_subscription('sensor_msgs/msg/LaserScan', '/scan', self.lidar_callback, qos_profile=QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))
+
+    def lidar_callback(self, msg):
+        self.get_logger().info(f'Received LaserScan {msg}')
 
 
 def main():
